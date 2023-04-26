0422ff9067a178f2e7d0a96c8a31d7c475c9fefe
Muazzamkhan95 <muazzamkhan95@gmail.com>
2023-04-26 15:51:54 +0500

    forget password using phone 3

--- app/Http/Controllers/Api/v1/AuthController.php
+++ app/Http/Controllers/Api/v1/AuthController.php
@@ -324,10 +324,18 @@ class AuthController extends Controller
         $user = User::where('phone', $request->phone)->first();
         // dd($user);
         if($user !== null ){
-            $token = env('TWILIO_AUTH_TOKEN', '47cc4930d2bd260a1645397ac38ebb75');
-            $twilio_sid = env('TWILIO_SID', 'ACe8c3d321c5982d30d06058179a4cbe84');
-            $twilio_verify_sid = env('TWILIO_VERIFY_SID', 'VA62f5835a3be4bf8df9317f5c6e366153');
-            $twilio = new Client($twilio_sid, $token);
+            $token = env('TWILIO_AUTH_TOKEN', '6b4241b1cd48f1297ffee4d3205c6213');
+        $twilio_sid = env('TWILIO_SID', 'ACe8c3d321c5982d30d06058179a4cbe84');
+        $twilio_verify_sid = env('TWILIO_VERIFY_SID', 'VA62f5835a3be4bf8df9317f5c6e366153');
+        // $token = env('TWILIO_AUTH_TOKEN','d013a35b70ecf8f1f092ad21d5955942');
+        // $twilio_sid = env('TWILIO_SID','ACf24d4b159d33a125bedafaf8127fee72');
+        // $twilio_verify_sid = env('TWILIO_VERIFY_SID','VA6f964f944769aaa4c6a48df14f9f01d3');
+        // $token = "d013a35b70ecf8f1f092ad21d5955942";
+        // $twilio_sid = "ACf24d4b159d33a125bedafaf8127fee72";
+        // $twilio_verify_sid = "VA6f964f944769aaa4c6a48df14f9f01d3" ;
+
+
+        $twilio = new Client($twilio_sid, $token);
             if ($twilio->verify->v2->services($twilio_verify_sid)
                 ->verifications
                 ->create($request['phone'], "sms")
